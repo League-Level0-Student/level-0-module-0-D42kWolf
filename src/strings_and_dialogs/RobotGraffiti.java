eb95cd34d6b4fcf7a5ea78b1574e3119749e3061
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-07 13:26:18 -0800

    RobotGraffiti RobotSquare

---
+++ src/strings_and_dialogs/RobotGraffiti.java
@@ -0,0 +1,17 @@
+package strings_and_dialogs;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+	public static void main(String[] args) {
+		Robot shuvi = new Robot();
+		shuvi.penDown();
+		shuvi.setSpeed(100);
+		shuvi.move(-150);
+		shuvi.turn(-90);
+		shuvi.move(50);
+		shuvi.turn(90);
+		shuvi.move(50);
+		shuvi.hide();
+	}}
+	
